51671e98441fbb0caaa746ba317a9cf43299f067
LorisDonovan <a7shky@gmail.com>
2020-10-12 22:32:21 +0545

    arbitrary length vector add

--- CUDA_Basics/src/vectorAdd/addVector.cu
+++ CUDA_Basics/src/vectorAdd/addVector.cu
@@ -2,57 +2,64 @@
 #include <device_launch_parameters.h>
 
 #include <iostream>
+#include <cassert>
 
 // CUDA kernel for vector addition
 // __global__ means this is called from the CPU, and runs on the GPU
 __global__ void addVector(int32_t N, float* x, float* y);
+__global__ void addVector_v2(int32_t N, float* x, float* y); // vector addition for arbitrary length N
 
 void Print(float* x, int32_t n);
+void VerifyResult(float* a, float* b, float* c, int32_t N);
 
 int main()
 {
-	const int32_t N = 1 << 20; // 2^20
+	constexpr int32_t N = 1 << 20; // 2^n
 	size_t bytes = N * sizeof(float);
 
 	// allocating host memory
-	float* h_X = (float*)malloc(bytes);
-	float* h_Y = (float*)malloc(bytes);
+	float* h_A = (float*)malloc(bytes);
+	float* h_B = (float*)malloc(bytes);
+	float* h_C = (float*)malloc(bytes);
 
-	float* d_X, * d_Y;
+	float* d_A, * d_B;
 	// allocating device memory
-	cudaMalloc(&d_X, bytes);
-	cudaMalloc(&d_Y, bytes);
+	cudaMalloc(&d_A, bytes);
+	cudaMalloc(&d_B, bytes);
 
 	// initialize array
 	for (int i = 0; i < N; i++)
 	{
-		h_X[i] = i * 2.0f;
-		h_Y[i] = i * 3.0f;
+		h_A[i] = i * 2.0f;
+		h_B[i] = i * 3.0f;
 	}
 
 	// copying from host to device
-	cudaMemcpy(d_X, h_X, bytes, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_Y, h_Y, bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_A, h_A, bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, h_B, bytes, cudaMemcpyHostToDevice);
 
 	int32_t blockSize = 256;
 	int32_t numBlocks = (N + blockSize - 1) / blockSize;
 
-	addVector<<<numBlocks, blockSize>>>(N, d_X, d_Y);
+	addVector<<<numBlocks, blockSize>>>(N, d_A, d_B);
 	// <<<no. of thread blocks, no. of threads in a block>>>
 	// because we passed only int this is only 1D 
 
-	cudaMemcpy(h_Y, d_Y, bytes, cudaMemcpyDeviceToHost);
+	//addVector_v2<<<256, 256>>>(N, d_A, d_B);
+
+	cudaMemcpy(h_C, d_B, bytes, cudaMemcpyDeviceToHost);
 
 	cudaDeviceSynchronize(); // to make cpu wait for the kernel to complete its operation
 
-	std::cout << "result:" << std::endl;
-	Print(h_Y, 5);
+	VerifyResult(h_A, h_B, h_C, N);
+	std::cout << "Vector Verified!" << std::endl;
 
 	// deallocating memory
-	free(h_X);
-	free(h_Y);
-	cudaFree(d_X);
-	cudaFree(d_Y);
+	free(h_A);
+	free(h_B);
+	free(h_C);
+	cudaFree(d_A);
+	cudaFree(d_B);
 
 	std::cin.get();
 	return 0;
@@ -63,6 +70,23 @@ __global__ void addVector(int32_t N, float* x, float* y)
 	int32_t tid = blockIdx.x * blockDim.x + threadIdx.x;
 	if (tid < N)
 		y[tid] = x[tid] + y[tid];
+}
+
+__global__ void addVector_v2(int32_t N, float* x, float* y)
+{
+	int32_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+	while (tid < N)
+	{
+		y[tid] = x[tid] + y[tid];
+		tid += blockDim.x * gridDim.x;
+	}
+}
+
+void VerifyResult(float* a, float* b, float* c, int32_t N)
+{
+	for (int i = 0; i < N; i++)
+		assert(c[i] == a[i] + b[i]);
 }
 
 void Print(float* x, int32_t n)
