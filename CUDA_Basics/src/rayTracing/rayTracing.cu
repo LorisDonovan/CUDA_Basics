2eabd906081d1c7479674d1c2a0839fb9cd90c5f
LorisDonovan <a7shky@gmail.com>
2020-10-14 17:27:29 +0545

    ray tracing without constant memory

---
+++ CUDA_Basics/src/rayTracing/rayTracing.cu
@@ -0,0 +1,126 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include <iostream>
+#include <random>
+#include <functional>
+
+#include "cpu_bitmap.h"
+
+#define DIM 1024
+#define INF 2e10f
+#define NUM_SPHERES 20
+#define rnd( x ) (x * rand() / RAND_MAX)
+
+struct Sphere
+{
+	float r, g, b;
+	float x, y, z;
+	float radius;
+
+	__device__ float Hit(float ox, float oy, float* n) // pixel (ox, oy)
+	{
+		float dx = ox - x;
+		float dy = oy - y;
+
+		if ((dx * dx + dy * dy) < (radius * radius))
+		{
+			float dz = std::sqrtf(radius * radius - dx * dx - dy * dy);
+			*n = dz / std::sqrtf(radius * radius);
+			return dz + z;
+		}
+		return -INF;
+	}
+};
+
+__global__ void Kernel(uint8_t* ptr, Sphere* s);
+void InitSphere(Sphere* s, std::function<float(float)> randFunc);
+
+int main()
+{
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
+
+	std::mt19937 gen(0);
+	std::uniform_real_distribution<float> distribution(0, 1);
+
+	CPUBitmap bitmap(DIM, DIM);
+	uint8_t* d_Bitmap;
+	Sphere* d_s;
+	
+	cudaMalloc(&d_Bitmap, bitmap.image_size());
+	cudaMalloc(&d_s, NUM_SPHERES * sizeof(Sphere));
+
+	Sphere* temp_s = (Sphere*)malloc(NUM_SPHERES * sizeof(Sphere));
+	InitSphere(temp_s, [&](float x) { return x * distribution(gen); });
+	cudaMemcpy(d_s, temp_s, NUM_SPHERES * sizeof(Sphere), cudaMemcpyHostToDevice);
+	free(temp_s);
+
+	dim3 grids(DIM / 16, DIM / 16);
+	dim3 threads(16, 16);
+	Kernel<<<grids, threads>>>(d_Bitmap, d_s);
+	cudaMemcpy(bitmap.get_ptr(), d_Bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost);
+
+	// get stop time, and display the timing results
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	float elapsedTime;
+	cudaEventElapsedTime(&elapsedTime, start, stop);
+	printf("Time to generate:  %3.1f ms\n", elapsedTime);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	bitmap.display_and_exit();
+
+	cudaFree(d_Bitmap);
+	cudaFree(d_s);
+	return 0;
+}
+
+__global__ void Kernel(uint8_t* ptr, Sphere* s)
+{
+	int32_t x = blockIdx.x * blockDim.x + threadIdx.x;
+	int32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+	int32_t offset = x + y * blockDim.x * gridDim.x;
+
+	float ox = x - DIM / 2;
+	float oy = y - DIM / 2;
+	float r = 0, g = 0, b = 0;
+	float maxz = -INF;
+
+	for (int i = 0; i < NUM_SPHERES; i++)
+	{
+		float n;
+		float t = s[i].Hit(ox, oy, &n);
+		if (t > maxz)
+		{
+			float fscale = n;
+			r = s[i].r * fscale;
+			g = s[i].g * fscale;
+			b = s[i].b * fscale;
+			maxz = t;
+		}
+	}
+
+	ptr[offset * 4 + 0] = (int32_t)(r * 255);
+	ptr[offset * 4 + 1] = (int32_t)(g * 255);
+	ptr[offset * 4 + 2] = (int32_t)(b * 255);
+	ptr[offset * 4 + 3] = 255;
+}
+
+void InitSphere(Sphere* s, std::function<float(float)> randFunc)
+{
+	for (int i = 0; i < NUM_SPHERES; i++)
+	{
+		s[i].r = randFunc(1.0f);
+		s[i].g = randFunc(1.0f);
+		s[i].b = randFunc(1.0f);
+		s[i].x = randFunc(1000.0f) - 500.0f;
+		s[i].y = randFunc(1000.0f) - 500.0f;
+		s[i].z = randFunc(1000.0f) - 500.0f;
+		s[i].radius = randFunc(100.0f) + 20.0f;
+	}
+}
